9aa4d0a1e28c8bfb780facee75f8e3e64f61bf34
Zhaodk <97621528@qq.com>
2026-08-06 08:37:36 +0800

    Use internal markdown for DeepTutorChat

--- SparkClient/Projects/Features/DeepTutorChat/Presentation/Rendering/DeepTutorMarkdownRenderer.swift
+++ SparkClient/Projects/Features/DeepTutorChat/Presentation/Rendering/DeepTutorMarkdownRenderer.swift
@@ -1,12 +1,11 @@
-import MarkdownUI
 import SwiftUI
 
 struct DeepTutorMarkdownRenderer: View {
     let markdown: String
 
     var body: some View {
-        MarkdownUI.Markdown(markdown)
-            .markdownTheme(MarkdownUI.Theme.gitHub)
+        Markdown(markdown)
+            .markdownTheme(.chatBubble(foreground: .primary))
             .font(.body)
             .frame(maxWidth: .infinity, alignment: .leading)
             .textSelection(.enabled)
